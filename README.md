172def2b73fc825646d0b6f9e74161360c4cf093
edwardluu <luuleminhthanh120493@gmail.com>
2025-10-01 18:05:06 +0700

    update notice

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# do-it-nad
-DoitNad
+# DoitNad
+DoitNad is rock, paper, scissors game play with bot.The game build on Monad testnet.
 
-game on monad
+![doitnad](./doitnad.png)
+
+# How to play
+- Connect wallet
+
+- Choice Rock, paper, scissors
+
+- Click button "Play"
+
+- Confirm transaction
+
+- Get your point
+
+# Features: 
+
+- Game play
+- Leaderboard
+- More .... in futures
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Rubik_Distressed } from "next/font/google";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 import { Web3Provider } from "@/provider/Web3Provider";
 import Header from "@/components/layout/header-app";
 import "./globals.css";
 
-
 const rubikDistressed = Rubik_Distressed({
   subsets: ["latin"],
-  weight: "400"
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -17,7 +16,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Do it Nad",
     description: "Rock paper scissors",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -25,6 +24,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  function calculateDaysFromToday() {
+    const timeDifference =
+      new Date("2025-10-10").getTime() - new Date().getTime();
+    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysDifference || 0;
+  }
+
   return (
     <html lang="en">
       <Web3Provider>
@@ -33,6 +40,13 @@ export default function RootLayout({
         >
           <div className="w-full flex items-center flex-col container bg-[#f8f4ef] z-10 rounded px-4">
             <Header />
+            <div className="w-full overflow-hidden whitespace-nowrap">
+              <p className="inline-block text-pink-500 text-2xl mt-1 animate-[marquee_10s_linear_infinite]">
+                DoItNad will officially shut down after{" "}
+                {calculateDaysFromToday()} days, 🏆 TOP 10 will receive 50 MON
+                as a special prize.
+              </p>
+            </div>
             <div className="h-[calc(100vh-80px)]  w-full flex flex-col container py-10">
               {children}
               <Analytics />
